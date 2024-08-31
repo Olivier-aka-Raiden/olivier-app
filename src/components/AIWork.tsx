972ce7f0073bc322085a006164b235f2e7d3883e
Olivier VILLEQUEY <olivier.villequey-ext@pole-emploi.fr>
2024-08-31 14:57:21 +0200

    added lora images

--- src/components/AIWork.tsx
+++ src/components/AIWork.tsx
@@ -5,6 +5,124 @@ import AIVideoMusicGallery from "./AIVideoMusicGallery";
 
 
 const AIWork: React.FC = () => {
+    const images = [
+        {
+            original: "/assets/img/lamborghini.webp",
+            thumbnail: "/assets/img/lamborghini.webp",
+        },
+        {
+            original: "/assets/img/black_lambo.webp",
+            thumbnail: "/assets/img/black_lambo.webp",
+        },
+        {
+            original: "/assets/img/wildlife.webp",
+            thumbnail: "/assets/img/wildlife.webp",
+        },
+        {
+            original: "/assets/img/ramen.webp",
+            thumbnail: "/assets/img/ramen.webp",
+        },
+        {
+            original: "/assets/img/BigMac.webp",
+            thumbnail: "/assets/img/BigMac.webp",
+        },
+        {
+            original: "/assets/img/beauty.webp",
+            thumbnail: "/assets/img/beauty.webp",
+        },
+        {
+            original: "/assets/img/cutie.webp",
+            thumbnail: "/assets/img/cutie.webp",
+        },
+        {
+            original: "/assets/img/demon.webp",
+            thumbnail: "/assets/img/demon.webp",
+        },
+        {
+            original: "/assets/img/hair.webp",
+            thumbnail: "/assets/img/hair.webp",
+        },
+        {
+            original: "/assets/img/koreanbeauty.webp",
+            thumbnail: "/assets/img/koreanbeauty.webp",
+        },
+        {
+            original: "/assets/img/masterpiece.webp",
+            thumbnail: "/assets/img/masterpiece.webp",
+        },
+        {
+            original: "/assets/img/model.webp",
+            thumbnail: "/assets/img/model.webp",
+        },
+        {
+            original: "/assets/img/mountainbiker.webp",
+            thumbnail: "/assets/img/mountainbiker.webp",
+        },
+        {
+            original: "/assets/img/mountainbiker_alps.webp",
+            thumbnail: "/assets/img/mountainbiker_alps.webp",
+        },
+        {
+            original: "/assets/img/mysteriousman.webp",
+            thumbnail: "/assets/img/mysteriousman.webp",
+        },
+        {
+            original: "/assets/img/stunningkorean.webp",
+            thumbnail: "/assets/img/stunningkorean.webp",
+        },
+    ];
+
+    const loraImages = [
+        {
+            original: "/assets/img/dr_strange_olivier.webp",
+            thumbnail: "/assets/img/dr_strange_olivier.webp",
+        },
+        {
+            original: "/assets/img/deadpool.webp",
+            thumbnail: "/assets/img/deadpool.webp",
+        },
+        {
+            original: "/assets/img/deadpool2.webp",
+            thumbnail: "/assets/img/deadpool2.webp",
+        },
+        {
+            original: "/assets/img/godfather.webp",
+            thumbnail: "/assets/img/godfather.webp",
+        },
+        {
+            original: "/assets/img/godfather_2.webp",
+            thumbnail: "/assets/img/godfather_2.webp",
+        },
+        {
+            original: "/assets/img/olivier_jedi2.webp",
+            thumbnail: "/assets/img/olivier_jedi2.webp",
+        },
+        {
+            original: "/assets/img/olivier_ww2.webp",
+            thumbnail: "/assets/img/olivier_ww2.webp",
+        },
+        {
+            original: "/assets/img/egypt.webp",
+            thumbnail: "/assets/img/egypt.webp",
+        },
+        {
+            original: "/assets/img/pyramides.webp",
+            thumbnail: "/assets/img/pyramides.webp",
+        },
+        {
+            original: "/assets/img/samurai2.webp",
+            thumbnail: "/assets/img/samurai2.webp",
+        },
+        {
+            original: "/assets/img/samurai3.webp",
+            thumbnail: "/assets/img/samurai3.webp",
+        },
+        {
+            original: "/assets/img/samurai4.webp",
+            thumbnail: "/assets/img/samurai4.webp",
+        },
+    ];
+
     return (
         <div>
             <h2 className="content__title">AI Art</h2>
@@ -15,7 +133,16 @@ const AIWork: React.FC = () => {
                         Explore a collection of my best images made with FLUX 1.dev model.
                     </p>
                     <div className="gallery">
-                        <AIImageGallery />
+                        <AIImageGallery images={images}/>
+                    </div>
+                </div>
+                <div>
+                    <h3 className="gallery-title">AI Finetuning</h3>
+                    <p className="gallery-description">
+                        Explore a collection of images from a model I trained to include my face.
+                    </p>
+                    <div className="gallery">
+                        <AIImageGallery images={loraImages}/>
                     </div>
                 </div>
                 <div>
